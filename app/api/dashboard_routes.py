73bb14f5ad90fde702a357a380cc3e9fa176fb4e
Kent <75441565+kburchill@users.noreply.github.com>
2021-04-26 14:24:32 -0600

    Completed math for dashboard

--- app/api/dashboard_routes.py
+++ app/api/dashboard_routes.py
@@ -17,14 +17,35 @@ def dashboard_data():
         my_transactions = TransactionExpense.query.filter(or_(
             TransactionExpense.lender_id == current_user.id, TransactionExpense.borrower_id == current_user.id)).all()
         print(my_transactions, "here ==========")
-        trans1amount = my_transactions[0].amount
         users_amounts = {}
         totals = {}
         owed = 0
         owe = 0
         total = 0
-        test change
+        for transaction in my_transactions:
 
-        return 'my_transactions'
+            amount = transaction.amount
+            if transaction.borrower_id == current_user.id:
+                # owe += amount
+                total -= amount
+                if transaction.lender_id in users_amounts:
+                    users_amounts[transaction.lender_id] -= amount
+                else:
+                    users_amounts[transaction.lender_id] = (amount * -1)
+            else:
+                # owed += amount
+                total += amount
+                if transaction.borrower_id in users_amounts:
+                    users_amounts[transaction.borrower_id] += amount
+                else:
+                    users_amounts[transaction.borrower_id] = amount
+        for user_id, user_amount in users_amounts.items():
+            if user_amount > 0:
+                owed += user_amount
+            else:
+                owe += user_amount
+        totals = {"owed": owed, "owe": owe, "total": total, "users_amounts": users_amounts}
+
+        return totals
 
     return {'errors': ['Unauthorized']}
