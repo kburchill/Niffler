72876ea66e8bdda1c78a3eda3756e33807f00620
sguttbinder <sguttbinder@gmail.com>
2021-04-26 16:36:41 -0700

    Added Comment Seeder File

--- app/seeds/transactions.py
+++ app/seeds/transactions.py
@@ -4,8 +4,8 @@ from app.models import db, Transaction
 def seed_transactions():
 
     demo1 = Transaction(
-        transaction_id="1",
-        lender_id="1",
+        transaction_id=1,
+        lender_id=1,
         amount=40,
         completed="",
         updated_at="2020-09-05 09:45:28",
@@ -14,8 +14,8 @@ def seed_transactions():
     db.session.add(demo1)
 
     demo2 = Transaction(
-        transaction_id="2",
-        lender_id="1",
+        transaction_id=2,
+        lender_id=1,
         amount=600,
         completed="",
         updated_at="2020-09-05 09:45:28",
@@ -24,8 +24,8 @@ def seed_transactions():
     db.session.add(demo2)
 
     demo3 = Transaction(
-        transaction_id="3",
-        lender_id="2",
+        transaction_id=3,
+        lender_id=2,
         amount=20,
         completed="",
         updated_at="2020-09-05 09:45:28",
@@ -34,8 +34,8 @@ def seed_transactions():
     db.session.add(demo3)
 
     demo4 = Transaction(
-        transaction_id="4",
-        lender_id="3",
+        transaction_id=4,
+        lender_id=3,
         amount=20,
         completed="",
         updated_at="2020-09-05 09:45:28",
@@ -44,8 +44,8 @@ def seed_transactions():
     db.session.add(demo4)
 
     demo5 = Transaction(
-        transaction_id="5",
-        lender_id="2",
+        transaction_id=5,
+        lender_id=2,
         amount=300,
         completed="",
         updated_at="2020-09-05 09:45:28",
