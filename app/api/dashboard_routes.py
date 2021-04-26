8363955cc8c7c47f12a14b6d08213e90054c0dfd
Kent <75441565+kburchill@users.noreply.github.com>
2021-04-26 15:37:48 -0600

    Working on group query

--- app/api/dashboard_routes.py
+++ app/api/dashboard_routes.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, session
-from app.models import User, db, TransactionExpense
+from flask import Blueprint
+from app.models import User, TransactionExpense, group_membership, Group
 from flask_login import current_user
-from sqlalchemy import or_
-
+from sqlalchemy import or_, create_engine
+from sqlalchemy.orm import sessionmaker, joinedload
+import os
 
 dashboard_routes = Blueprint("dashboard", __name__)
+db_url = os.environ.get("DATABASE_URL")
+engine = create_engine(db_url)
+SessionFactory = sessionmaker(bind=engine)
+session = SessionFactory()
 
 
 @dashboard_routes.route("/")
@@ -16,24 +21,19 @@ def dashboard_data():
     if current_user.is_authenticated:
         my_transactions = TransactionExpense.query.filter(or_(
             TransactionExpense.lender_id == current_user.id, TransactionExpense.borrower_id == current_user.id)).all()
-        print(my_transactions, "here ==========")
         users_amounts = {}
-        totals = {}
         owed = 0
         owe = 0
         total = 0
         for transaction in my_transactions:
-
             amount = transaction.amount
             if transaction.borrower_id == current_user.id:
-                # owe += amount
                 total -= amount
                 if transaction.lender_id in users_amounts:
                     users_amounts[transaction.lender_id] -= amount
                 else:
                     users_amounts[transaction.lender_id] = (amount * -1)
             else:
-                # owed += amount
                 total += amount
                 if transaction.borrower_id in users_amounts:
                     users_amounts[transaction.borrower_id] += amount
@@ -44,8 +44,12 @@ def dashboard_data():
                 owed += user_amount
             else:
                 owe += user_amount
-        totals = {"owed": owed, "owe": owe, "total": total, "users_amounts": users_amounts}
-
-        return totals
+        printthis = session.query(group_membership).options(joinedload(groups).filter(
+            group_membership.c.user_id == current_user.id).all()
+        print(printthis, "here ============")
+        current_user_data = {"owed": owed, "owe": owe,
+                             "total": total, "users_amounts": users_amounts}
+        return current_user_data
+#result = session.query(Customer).join(Invoice).filter(Invoice.amount == 8500)
 
     return {'errors': ['Unauthorized']}
