7533009f1557cd6b50d327cbac1cfd6930cec7f8
Kent <75441565+kburchill@users.noreply.github.com>
2021-04-28 13:35:13 -0600

    Get route for group data organized

--- app/api/group_routes.py
+++ app/api/group_routes.py
@@ -14,46 +14,40 @@ def group_data(group_id):
     Provides transactions for current group id
     """
     if (current_user.is_authenticated):
-        print("user====is====authenticated")
+        # print("user====is====authenticated")
         if any(group.id == int(group_id) for group in current_user.groups):
+            #queries
             group_transactions = Transaction.query.filter(Transaction.group_id == group_id).all()
-            defined_transactions = {}
-            for each_transaction in group_transactions:
-                defined_transactions[each_transaction.id] = each_transaction.expenses
             group_data = Group.query.get(int(group_id))
-            print(defined_transactions, "============")
-            # {1: <all transactions associated>}
-            # {(for (1) get payer id/name:)
-            # (for each transaction, get lent amount total, {and each user id/name/lent amount}}
-            # return_me_to_frontend = {}
-            # for transaction in group_transactions:
-            #     for expense in transaction:
-            #         name = name for expense.lender_id in group_data
-            #         Debtor_info = {
-            #             expense.lender_id: {"name": [name for expense.lender_id in group_data], "amount": expense.amount}
-            #         }
-            #     return_me_to_frontend[transaction.id] = {
-                    # {
-                    # Payer: Kent, Amount: 100, Date: 5/5/2020 Debtors: {user_id(1): {name: Min Ki, amount: 25}, user_id(2): { name: Steve, amount: 25}
-                    # }
-                #     "Payer": transaction.payer, "Amount": transaction.amount, "Date": transaction.date,
-                #     "Debtors": {
+            #create variables to organize data
+            return_me_to_frontend = {}
 
 
-                #     }
-                # }
 
-## What do we want to display?
-## Transaction description, paid amount, lent amount, User and their expense amount
+            #Create list of expenses associated with transaction
+            for transaction in group_transactions:
+                all_expenses = [*transaction.expenses]
+                Debtor_info = []
+                for expense in all_expenses:
+                    a_user = next(user for user in group_data.users if expense.borrower_id == user.id)
+                    Debtor_info.append({a_user.id: {"name": a_user.first_name, "Amount": expense.amount}})
+                    # print(Debtor_info)
+                return_me_to_frontend[transaction.id] = {
+                    "Payer": transaction.payer_id, "Amount": transaction.paid_amount, "Date": transaction.expense_date,
+                    "Debtors": Debtor_info
+                }
+            print(return_me_to_frontend)
+# # What do we want to display?
+# # Transaction description, paid amount, lent amount, User and their expense amount
 
-## What do we have access to?
-## Transaction description (group_transaction[0].description)
-## Paid amount >> add onto database
-## lent amount >> total up from each transactions
+# # What do we have access to?
+# # Transaction description (group_transaction[0].description)
+# # Paid amount >> add onto database
+# # lent amount >> total up from each transactions
 # {
 # (transaction)1: {Payer: Kent, Amount: 100, Date: 5/5/2020 Debtors: {user_id(1): {name: Min Ki, amount: 25}, user_id(2): { name: Steve, amount: 25}}
 # }
-#
 
-##
+
+# #
     return "5"
