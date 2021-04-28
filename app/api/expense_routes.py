031ff536adfd323cba9845987078b9c86fb1f717
minkikim98 <minkikim98@gmail.com>
2021-04-28 14:45:04 -0700

    transaction creation with simple form works

--- app/api/expense_routes.py
+++ app/api/expense_routes.py
@@ -1,14 +1,55 @@
 from flask import Blueprint, request
 from flask_login import current_user
 from app.forms import ExpenseForm
+from app.models import Transaction, TransactionExpense, db
+from datetime import date
 
 
 expense_routes = Blueprint("expenses", __name__)
+
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f"{field} : {error}")
+    return errorMessages
 
 @expense_routes.route("/", methods=["POST"])
 def create_expense():
     form = ExpenseForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        print("validated successfully")
-    return {}
+        # Create the new transaction.
+        new_transaction = Transaction(
+            group_id=form.data["group_id"],
+            payer_id=form.data["payer_id"],
+            paid_amount=form.data["amount"],
+            description=form.data["description"],
+            expense_date=form.data["expense_date"],
+            completed=False,
+            created_at=date.today()
+        )
+        db.session.add(new_transaction) 
+        db.session.commit()
+
+        # For each debtor, create a new transaction expense.
+        payed_amount = form.data["amount"]
+        split_by = len(form.data["debtors"])
+        remainder = payed_amount % split_by
+        debtor_pays = (payed_amount - remainder) / split_by
+        for debtor in form.data["debtors"]:
+            new_expense = TransactionExpense(
+                transaction_id=new_transaction.id,
+                lender_id=form.data["payer_id"],
+                borrower_id=debtor,
+                amount=debtor_pays,
+                completed=False,
+                updated_at=date.today()
+            )
+            db.session.add(new_expense) 
+        db.session.commit()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
--- app/forms/expense_form.py
+++ app/forms/expense_form.py
@@ -1,12 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, StringField, IntegerField, DateField, SelectMultipleField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+def amount_correct(form, field):
+    print("Checking if payed amount is not zero and not negative", field.data)
+    amount = field.data
+    if amount < 1:
+        raise ValidationError("Incorrect payed amount.")
 
 class ExpenseForm(FlaskForm):
-    amount = IntegerField("amount", validators=[DataRequired()])
+    amount = IntegerField("amount", validators=[DataRequired(), amount_correct])
     payer_id = IntegerField("payer_id", validators=[DataRequired()])
     description = TextAreaField("description", validators=[DataRequired()])
     group_id = IntegerField("group_id")
     expense_date = DateField("expense_date", validators=[DataRequired()])
+    # Will eventually have to dynamically generate choices.
     debtors = SelectMultipleField("debtors", coerce=int, choices=[(1, "Demolition"), (2, "Harry"), (3, "Hermione"), (4, "Ronald")])
 
