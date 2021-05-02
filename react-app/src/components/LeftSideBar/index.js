afee8ad586cab7427ea25f38bc5646bda9fa6dad
sguttbinder <sguttbinder@gmail.com>
2021-05-01 17:32:04 -0700

    CSS Updates

--- react-app/src/components/LeftSideBar/index.js
+++ react-app/src/components/LeftSideBar/index.js
@@ -9,23 +9,33 @@ const LeftSideBar = () => {
     const userGroups = useSelector(state => state.userData.groups);
 
     return (
-        <div className="left-sidebar">
-            <div className="left-sidebar__main">
-                <NavLink to="/dashboard" exact={true} activeClassName="active">
-                    Dashboard
-                </NavLink>
-            </div>
-                
-            <div className="left-sidebar__groups">
-                {/* { typeof userGroups} */}
-                {userGroups && Object.entries(userGroups).map(([group_id, group_name]) => (
-                    <NavLink key={group_id} to={`/groups/${group_id}`} exact={true} activeClassName="active">
-                        {group_name}
-                    </NavLink>
-                ))}
-            </div>
+      <div className="left-sidebar">
+        <div className="left-sidebar__main sidebar-link">
+          <NavLink
+            to="/dashboard"
+            exact={true}
+            activeClassName="sidebar-link"
+          >
+            Dashboard
+          </NavLink>
         </div>
-    )
+
+        <div className="left-sidebar__groups sidebar-link">
+          {/* { typeof userGroups} */}
+          {userGroups &&
+            Object.entries(userGroups).map(([group_id, group_name]) => (
+              <NavLink
+                key={group_id}
+                to={`/groups/${group_id}`}
+                exact={true}
+                activeClassName="sidebar-link"
+              >
+                {group_name}
+              </NavLink>
+            ))}
+        </div>
+      </div>
+    );
 }
 
 export default LeftSideBar;
