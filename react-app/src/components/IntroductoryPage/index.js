2c3681b1cc8f0a408d78244ae0e4a268a5d02273
sguttbinder <sguttbinder@gmail.com>
2021-05-03 08:39:13 -0700

    Profiles In Progress

--- react-app/src/components/IntroductoryPage/index.js
+++ react-app/src/components/IntroductoryPage/index.js
@@ -1,19 +1,55 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
-import HeaderBar from "../HeaderBar";
-
+import HeaderBar from '../HeaderBar';
+import './IntroductoryPage.css';
 
 const IntroductoryPage = () => {
-    return (
-      <div className="main-body">
-        <HeaderBar />
-        <div className="introductory-page__image-container">
-          <div>
-            <img src="images/NifflerBankLanding.jpg" width="100%"></img>
-          </div>
+  return (
+    <div className="main-body">
+      <HeaderBar />
+      <div className="introductory-page__image-container">
+        <div>
+          <img src="images/NifflerBankLanding.jpg" width="100%"></img>
         </div>
       </div>
-    );
-}
+      <div className="team">
+        {/* <div className ="person">
+              
+              <div className="profile-pic">
+              </div>
+              <div className="profile-name">
+              Kent 
+              </div>
+              <div className="linkedin">
+              </div>
+              <div className="github">
+              </div>
+            <div className ="person">
+              
+              <div className="profile-pic">
+              </div>
+              <div className="profile-name">
+              
+              </div>
+              <div className="linkedin">
+              </div>
+              <div className="github">
+              </div>
+            <div className ="person">
+              
+              <div className="profile-pic">
+              </div>
+              <div className="profile-name">
+              
+              </div>
+              <div className="linkedin">
+              </div>
+              <div className="github">
+              </div> */}
+        {/* </div> */}
+      </div>
+    </div>
+  );
+};
 
 export default IntroductoryPage;
